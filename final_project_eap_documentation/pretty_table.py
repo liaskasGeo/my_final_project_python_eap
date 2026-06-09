3f22b10f070c0ca5178eedeeea69724ccf9d8fad
Georgios Liaskas <liaskas.geo@gmail.com>
2026-06-09 20:51:04 +0300

    Add files via upload

---
+++ final_project_eap_documentation/pretty_table.py
@@ -0,0 +1,11 @@
+import prettytable
+from prettytable import PrettyTable
+
+table = PrettyTable()
+print(table)
+
+table.add_column("Pokemon Name",["Pikachu","Squirtle","Charmander"])
+table.add_column("Type",["Electric","Water","Fire"])
+table.align = "l" #Align Left
+
+print(table)
